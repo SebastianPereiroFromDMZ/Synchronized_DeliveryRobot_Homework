8c0005ba4ac89b87b5fb18c2defc0f2397b9b2ee
SebastianPereiro <kuzmin_konstantin@bk.ru>
2023-03-30 22:16:16 +0300

    commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,17 @@ public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        creatingStreams("RLRFR", 100, 1000);
+        List<Thread> threadList = creatingListThreads(1000, "RLRFR", 100);
+
+        Thread PrintMaxValue = printingTheMaximum();
+        PrintMaxValue.start();
+
+        startingThreads(threadList);
+
+        PrintMaxValue.interrupt();
+
 
         LinkedHashMap<Integer, Integer> sortedMap = sort();
 
@@ -17,7 +25,47 @@ public class Main {
         listOfOtherSizes(sortedMap);
     }
 
-    public static LinkedHashMap<Integer, Integer> sort(){
+    public static Thread printingTheMaximum(){
+        return new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    Map.Entry<Integer, Integer> max = sizeToFreq
+                            .entrySet()
+                            .stream()
+                            .max(Map.Entry.comparingByKey())
+                            .get();
+                    System.out.println("Максимальное значение: " + max.getKey() + " встретилось: "
+                            + max.getValue() + " раз(а)");
+                }
+            }
+        });
+    }
+
+    public static void startingThreads(List<Thread> threads) throws InterruptedException {
+        for (Thread thread : threads) {
+            thread.start();
+            Thread.sleep(1);
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
+
+    public static List<Thread> creatingListThreads(int countThreads, String route, int lengthWay){
+        List<Thread> threadList = new ArrayList<>();
+        for (int i = 0; i < countThreads; i++) {
+            threadList.add(creatingThread(route, lengthWay));
+        }
+        return threadList;
+    }
+
+    public static LinkedHashMap<Integer, Integer> sort() {
         return sizeToFreq.entrySet()
                 .stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))
@@ -29,31 +77,32 @@ public class Main {
     }
 
 
-    public static void creatingStreams(String routes, int lengthWays, int countThreads){
-        for (int i = 0; i < countThreads; i++) {
-            new Thread(() -> {
-                String text = generateRoute(routes, lengthWays);
-                Map<Character, Integer> map = new HashMap<>();
-                for (int k = 0; k < text.length(); k++) {
-                    Integer n = map.get(text.charAt(k));
-                    if (n == null) map.put(text.charAt(k), 1);
-                    else map.put(text.charAt(k), n + 1);
-                }
+    public static Thread creatingThread(String routes, int lengthWays) {
 
-                synchronized (sizeToFreq) {
-                    Integer characterChecker = map.get('R');//берем число повторений R допустим 76, это число становиться ключом для статичной мапы
-                    Integer firstRepeat = 1;//это будет значение (количество повторений R в потоках здесь 1000 потоков), далее будем фиксировать количество повторяющихся
-                    if (!sizeToFreq.containsKey(characterChecker)) {//если мапа не содержит количесво повторений (76) в потоке
-                        sizeToFreq.put(characterChecker, firstRepeat);//ложим это количество повторений (ключ) в ячейку а в значении количество этих повторений,
-                        // у нас на данный момент это 1
-                    } else {
-                        Integer repeatedRepetitions = sizeToFreq.get(characterChecker);//фиксируем количество повторений в переменную например 4
-                        repeatedRepetitions = repeatedRepetitions + 1;//плюсуем 1 повторение
-                        sizeToFreq.put(characterChecker, repeatedRepetitions);//изменяем количество повторений в мапе при сохраненном ключе
-                    }
+        return new Thread(() -> {
+            String text = generateRoute(routes, lengthWays);
+            Map<Character, Integer> map = new HashMap<>();
+            for (int k = 0; k < text.length(); k++) {
+                Integer n = map.get(text.charAt(k));
+                if (n == null) map.put(text.charAt(k), 1);
+                else map.put(text.charAt(k), n + 1);
+            }
+
+            synchronized (sizeToFreq) {
+                Integer characterChecker = map.get('R');//берем число повторений R допустим 76, это число становиться ключом для статичной мапы
+                Integer firstRepeat = 1;//это будет значение (количество повторений R в потоках здесь 1000 потоков), далее будем фиксировать количество повторяющихся
+                if (!sizeToFreq.containsKey(characterChecker)) {//если мапа не содержит количесво повторений (76) в потоке
+                    sizeToFreq.put(characterChecker, firstRepeat);//ложим это количество повторений (ключ) в ячейку а в значении количество этих повторений,
+                    // у нас на данный момент это 1
+
+                } else {
+                    Integer repeatedRepetitions = sizeToFreq.get(characterChecker);//фиксируем количество повторений в переменную например 4
+                    repeatedRepetitions = repeatedRepetitions + 1;//плюсуем 1 повторение
+                    sizeToFreq.put(characterChecker, repeatedRepetitions);//изменяем количество повторений в мапе при сохраненном ключе
                 }
-            }).start();
-        }
+                sizeToFreq.notify();
+            }
+        });
     }
 
     public static void giveTheFirstElementAndDeleteIt(LinkedHashMap<Integer, Integer> map) {
@@ -88,4 +137,3 @@ public class Main {
         return route.toString();
     }
 }
-
